80d1e7de5084574c8a2eb39339d1e1dd27db49e1
Robert <robert.morgan0703@gmail.com>
2023-04-01 16:35:48 +0100

    setup api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 function App() {
+
+  const [ mhwData, setMhwData ] = useState()
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await axios.get('https://mhw-db.com/monsters')
+        setMhwData(data)
+        console.log({ mhwData })
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getData()
+  }, [])
+  
+
   return (
-    <h1>MHW Random</h1>
+    <h1>bottom text</h1>
   );
 }
 
